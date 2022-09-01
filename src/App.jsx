c9dd8f0af7aef17f0a406c383dfa5bb300de4c2a
AlanJuarezzz3 <alan.juarez_roman@hotmail.com>
2022-09-01 14:39:52 -0300

    material UI

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import ResponsiveAppBar from './Components/Navbar';
+
+function App(){
+  return(  
+    <div className="App">
+     <ResponsiveAppBar />
+     
+   </div>
+  );
+}
+
+export default App;
