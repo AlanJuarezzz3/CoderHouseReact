0cbefee1abb4e0d3007eaa799e946ba0f3de6dae
AlanJuarezzz3 <alan.juarez_roman@hotmail.com>
2022-09-06 15:26:11 -0300

    actualizado

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import ResponsiveAppBar from './Components/Navbar';
-import Mensaje from './Components/ItemListContainer';
+import ItemListContainer from './Components/ItemListContainer';
 
 const App = () => {
   return(  
     <div className='Nav'>
      <ResponsiveAppBar />
-     <Mensaje descripcion="Ninja" precio="$30.000"/>
-     <Mensaje descripcion ="Honda" precio ="$10.000"/>
+     <ItemListContainer descripcion="Ninja" precio="$30.000"/>
+     <ItemListContainer descripcion ="Honda" precio ="$10.000"/>
     </div>
   );
 }
