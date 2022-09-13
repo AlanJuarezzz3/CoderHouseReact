9a4e65281a575e61f34f86d575cd9c3f6d76a1ba
AlanJuarezzz3 <alan.juarez_roman@hotmail.com>
2022-09-13 18:59:13 -0300

    actualizacion

--- src/Components/itemsList/Item.jsx
+++ src/Components/itemsList/Item.jsx
@@ -1,17 +1,28 @@
 import React from 'react';
 import Contador from './ItemCount';
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import Typography from '@mui/material/Typography';
+
 const Item = ({product}) => {
   return (
-    <>
-    <h2>{product.nombre}</h2>
-    <div>{product.precio}</div>
-    <div>{product.detalle}</div>
-    <img src={product.img} alt ="imagen1" width= {150}/>
-    <Contador initial ={1} stock ={10}/>
-    <hr></hr>
-    </>
-  )
+    <Card sx={{ maxWidth: 345 }}>
+      <img src={product.img} alt ="imagen1" width= {150}/>
+      <CardContent>
+        <Typography gutterBottom variant="h5" component="div">
+        {product.nombre}
+        </Typography>
+        <Typography variant="body2" color="text.secondary">
+        <div>{product.detalle}</div>
+        </Typography>
+      </CardContent>
+      <CardActions>
+      <Contador initial ={1} stock ={10}/>
+      </CardActions>
+      <hr></hr>
+    </Card>
+  );
 }
-
 
 export default Item;
