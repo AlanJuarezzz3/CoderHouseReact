57147f2f00f55418aab04978a3655b174576cf10
AlanJuarezzz3 <alan.juarez_roman@hotmail.com>
2022-09-19 15:28:40 -0300

    actualizado

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import ResponsiveAppBar from './Components/Navbar';
 import ItemListContainer from './Components/itemsList/ItemListContainer';
 import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import ItemDetailContainer from './Components/itemsList/ItemDetailContainer';
 
 const App = () => {
   return( 
@@ -12,6 +13,7 @@ const App = () => {
      <Routes>
       <Route path='/' element ={<ItemListContainer/>}/>
       <Route path ='category/:categoria' element={<ItemListContainer/>}/>
+      <Route path ='item/:id' element={<ItemDetailContainer/>}/>
      </Routes>
     
     </Router>
